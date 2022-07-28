23a1b37955ba4c53cf154f2edc90cf27dea800d1
idris <buhkunmii@gmail.com>
2022-07-28 16:47:10 +0100

    dddd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,29 +5,23 @@ import product from "./Homeproduct";
 import "./Homeproduct.css";
 import Mersh from "./Mersh";
 
-
-
-
-
-
 const MainContent = () => {
-  
     let productArr = [];
 
     for (let index = 0; index < 8; index++) {
         productArr.push(product[index]);
     }
-    
 
     const listItems = productArr.map((item) => (
         <div className="col container col-lg-3 my-4 md-4 " key={item.id}>
             <div className="snip1268 shadow-lg">
                 <div className="image">
-                    <img src={item.thumb} alt="" className=" p-1"/>
+                    <img src={item.thumb} alt="" className=" p-1 p-2" />
 
                     <div className="add-to-cart mb-3">
                         <img src={item.view} className="pt-2" alt="" />
-                        <Link style={{ color: ' ', textDecoration: "none"}}
+                        <Link
+                            style={{ color: " ", textDecoration: "none" }}
                             to={`/product/${item.category}/${item.product_name}`}
                         >
                             {" "}
@@ -48,26 +42,18 @@ const MainContent = () => {
         </div>
     ));
     return (
-        
         <div className="">
+            <Carousel />
+            <div class=" container mt-3 mb-4">
+                <h2>New Waves</h2>
 
-       <Carousel />
-        <div class=" container mt-3 mb-4">
-       
-            <h2>New Waves</h2>
-
-            <div className="row container">{listItems}</div>
+                <div className="row container">{listItems}</div>
             </div>
-            
-          
-           
+
             <div className="container-fluid">
-              <Mersh />
-           
+                <Mersh />
+            </div>
         </div>
-       
-        </div>
-        
     );
 };
 export default MainContent;
